152e2cda6a9608b6caf1a9190b975fc8a833ae77
Varun Vachhar <varunvachhar@gmail.com>
2015-06-15 10:55:48 -0400

    added footer and a bit more cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -12,14 +12,6 @@ var styles = [
   './css/styles.css'
 ];
 
-
-gulp.task('gh-deploy', function() {
-  return gulp.src('./build/**/*')
-    .pipe(ghPages({
-      remoteUrl: 'git@github.com:winkerVSbecks/triangle.life.git'
-    }));
-});
-
 gulp.task('css', function() {
   return gulp.src(styles)
           .pipe(concat('styles.min.css'))
@@ -36,7 +28,7 @@ gulp.task('img', function() {
 
 gulp.task('misc', function() {
   return gulp.src(['./CNAME', './LICENSE', './google34316d718c8bcc05.html'])
-          .pipe(gulp.dest('./build/img'))
+          .pipe(gulp.dest('./build'))
           .pipe(connect.reload());
 });
 
@@ -47,12 +39,9 @@ gulp.task('index', function() {
           .pipe(connect.reload());
 });
 
+gulp.task('build', ['index', 'img', 'css', 'misc']);
 
-gulp.task('default', [
-  'index',
-  'img',
-  'css'
-], function() {
+gulp.task('default', ['build'], function() {
   // Start a server
   connect.server({
     root: './build',
@@ -68,4 +57,11 @@ gulp.task('default', [
   watch('./index.html', function () {
     gulp.start('index');
   });
+});
+
+gulp.task('gh-deploy', ['build'] function() {
+  return gulp.src('./build/**/*')
+    .pipe(ghPages({
+      remoteUrl: 'git@github.com:winkerVSbecks/triangle.life.git'
+    }));
 });
